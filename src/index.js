2bf47d730977b95956a6bff2ff0d76d6af49e080
Oleg <hellreebok@gmail.com>
2023-04-15 09:23:51 +0300

    updated basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
     <React.StrictMode>
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <BrowserRouter basename="/testtaskgoit">
+                <BrowserRouter basename="/testTaskGoIt">
                     <App />
                 </BrowserRouter>
             </PersistGate>
